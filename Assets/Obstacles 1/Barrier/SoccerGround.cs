f66f666ab497f3a10830b08f4d356e61aa9a32c5
IrvingWu <enweiwu@usc.edu>
2021-02-22 16:19:47 +0800

    Build a basic map

---
+++ Assets/Obstacles 1/Barrier/SoccerGround.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoccerGround : MonoBehaviour
+{
+    
+    public static bool triggerSoccer = false;
+
+    private void OnCollisionEnter2D(Collision2D other)
+    {
+        // GameObject stilt = GameObject.FindWithTag("Stilt");
+        BoxCollider2D stilt = other.collider.GetComponent<BoxCollider2D>();
+        if (stilt != null && stilt.CompareTag("Stilt"))
+        {
+            triggerSoccer = true;
+        }
+        Debug.Log("trigger is ");
+        Debug.Log(other.collider);
+    }
+    
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
