645cdaddb33c24242e27b223e7a36a0ff5efae7c
Yiran Zhang <yzhang60@usc.edu>
2021-02-22 21:21:44 -0800

    tutorial scence

---
+++ Assets/PanelController.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PanelController : MonoBehaviour
+{
+    public GameObject panel;
+    int count;
+    public void control()
+    {
+        count++;
+        if (count % 2 == 1)
+        {
+            panel.gameObject.SetActive(true);
+        }
+        else
+        {
+            panel.gameObject.SetActive(false);
+        }
+    }
+}
