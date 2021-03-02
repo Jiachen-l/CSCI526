742300e00f6e1b4037097b59abe07191da2e8c30
xmNiu-Sam <niux@usc.edu>
2021-03-01 20:51:36 -0800

    Add checkpoints when collide with barriers on level 1.

--- Assets/c#/otherGameOverController.cs
+++ Assets/c#/otherGameOverController.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+
 public class otherGameOverController : MonoBehaviour
 {
 
@@ -10,10 +11,13 @@ public class otherGameOverController : MonoBehaviour
     public GameObject pauseButton;
 
     public GameObject retryButton;
+
+    public GameObject[] checkPoints;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        checkPoints = GameObject.FindGameObjectsWithTag("checkpoint");
     }
 
     // Update is called once per frame
@@ -23,9 +27,29 @@ public class otherGameOverController : MonoBehaviour
     }
     void OnCollisionEnter2D(Collision2D other) {
         if (other.gameObject.name == "bone_3" || other.gameObject.name == "bone_4") {
-            gameOverMenu.SetActive(true);
-            retryButton.SetActive(false);
-            pauseButton.SetActive(false);
+            // gameOverMenu.SetActive(true);
+            // retryButton.SetActive(false);
+            // pauseButton.SetActive(false);
+            int i;
+            for (i = 0; i < checkPoints.Length - 1; i++)
+            {
+                if (
+                    other.gameObject.transform.position.x >= checkPoints[i].transform.position.x &&
+                    other.gameObject.transform.position.x < checkPoints[i + 1].transform.position.x)
+                {
+                    GameObject.Find("bone_1").transform.position = checkPoints[i].transform.position;
+                    GameObject.Find("bone_3").transform.position = checkPoints[i].transform.position;
+                    GameObject.Find("bone_4").transform.position = checkPoints[i].transform.position;
+                    break;
+                }
+            }
+            if (i == checkPoints.Length - 1)
+            {
+                GameObject.Find("bone_1").transform.position = checkPoints[i].transform.position;
+                GameObject.Find("bone_3").transform.position = checkPoints[i].transform.position;
+                GameObject.Find("bone_4").transform.position = checkPoints[i].transform.position;
+            }
+
         }
     }
 }
