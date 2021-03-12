c1579138535a941c80ba9774cdf807bb6dbe3ee0
XingleiRen <52548847+XingleiRen@users.noreply.github.com>
2021-03-12 13:17:45 -0800

    added coin collection function

---
+++ Assets/c#/PickupCoins.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PickupCoins : MonoBehaviour
+{
+    private Inventory inventory;
+    public GameObject itemButton;
+
+    private void Start()
+    {
+        inventory = GameObject.FindGameObjectWithTag("Player").GetComponent<Inventory>();
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            for(int i = 0; i< inventory.slots.Length; i++)
+            {
+                if (inventory.isFull[i] == false)
+                {
+                    //ITEM CAN BE ADDED TO INVENTORY
+                    inventory.isFull[i] = true;
+                    Instantiate(itemButton, inventory.slots[i].transform,false);
+                    Destroy(gameObject);
+                    break;
+                }
+            }
+        }
+    }
+}
---
+++ Assets/c#/ReawardsUIdisplay.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ReawardsUIdisplay : MonoBehaviour
+{
+    private int health = 5;
+    public Text healthText;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        healthText.text = "HEALTH:" + health;
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            health--;
+        }
+    }
+}
