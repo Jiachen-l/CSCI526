69726b35d53cc2dee5dee19d940e35db38d20a40
ereret2 <lyuy@usc.edu>
2021-04-20 15:32:32 -0700

    add shake animation

---
+++ Assets/c#/shake.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class shake : MonoBehaviour
+{
+    public Animator can_anim;
+
+    public void can_shake() {
+        Debug.Log("enter can_shake");
+        can_anim.SetTrigger("shake");
+        Debug.Log("leave can_shake");
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
