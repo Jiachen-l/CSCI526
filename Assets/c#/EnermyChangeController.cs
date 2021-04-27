600d7e614bb48e66ac08d73ddd1899b8192b0635
xmNiu-Sam <niux@usc.edu>
2021-04-27 16:05:52 -0700

    add enemy bonus effect

--- Assets/c#/EnermyChangeController.cs
+++ Assets/c#/EnermyChangeController.cs
@@ -9,8 +9,22 @@ public class EnermyChangeController : MonoBehaviour
     static public int characterIndex_2;
 
     public bool changeCharacterActive;
+
+    // flash effect
+    private SpriteRenderer bodySprite;
+    private SpriteRenderer headSprite;
+
+    private bool flashActive;
+    public float flashLength = 0.5f;
+    public float flashCounter;
     void Start()
     {
+        if (GameObject.Find("XRpro") != null)
+        {
+            bodySprite = GameObject.Find("Body").GetComponent<SpriteRenderer>();
+            headSprite = GameObject.Find("Head").GetComponent<SpriteRenderer>();
+        }
+
         characterIndex_2 = 0;
         characterCollection[0].gameObject.SetActive(true);
         for (int i = 1; i < characterCollection.Length; i++)
@@ -25,6 +39,114 @@ public class EnermyChangeController : MonoBehaviour
     void Update()
     {
         ChangeCharacterFunction();
+        if (flashActive)
+        {
+            if (GameObject.Find("XRpro") != null)
+            {
+                if (flashCounter > flashLength * .95f)
+                {
+                    bodySprite.color = Color.green;
+                    headSprite.color = Color.green;
+                }
+                else if (flashCounter > flashLength * .90f)
+                {
+                    bodySprite.color = Color.white;
+                    headSprite.color = Color.white;
+                }
+                else if (flashCounter > flashLength * .85f)
+                {
+                    bodySprite.color = Color.green;
+                    headSprite.color = Color.green;
+                }
+                else if (flashCounter > flashLength * .80f)
+                {
+                    bodySprite.color = Color.white;
+                    headSprite.color = Color.white;
+                }
+                else if (flashCounter > flashLength * .75f)
+                {
+                    bodySprite.color = Color.green;
+                    headSprite.color = Color.green;
+                }
+                else if (flashCounter > flashLength * .70f)
+                {
+                    bodySprite.color = Color.white;
+                    headSprite.color = Color.white;
+                }
+                else if (flashCounter > flashLength * .65f)
+                {
+                    bodySprite.color = Color.green;
+                    headSprite.color = Color.green;
+                }
+                else if (flashCounter > flashLength * .60f)
+                {
+                    bodySprite.color = Color.white;
+                    headSprite.color = Color.white;
+                }
+                else if (flashCounter > flashLength * .55f)
+                {
+                    bodySprite.color = Color.green;
+                    headSprite.color = Color.green;
+                }
+                else if (flashCounter > flashLength * .50f)
+                {
+                    bodySprite.color = Color.white;
+                    headSprite.color = Color.white;
+                }
+                else if (flashCounter > flashLength * .45f)
+                {
+                    bodySprite.color = Color.green;
+                    headSprite.color = Color.green;
+                }
+                else if (flashCounter > flashLength * .40f)
+                {
+                    bodySprite.color = Color.white;
+                    headSprite.color = Color.white;
+                }
+                else if (flashCounter > flashLength * .35f)
+                {
+                    bodySprite.color = Color.green;
+                    headSprite.color = Color.green;
+                }
+                else if (flashCounter > flashLength * .30f)
+                {
+                    bodySprite.color = Color.white;
+                    headSprite.color = Color.white;
+                }
+                else if (flashCounter > flashLength * .25f)
+                {
+                    bodySprite.color = Color.green;
+                    headSprite.color = Color.green;
+                }
+                else if (flashCounter > flashLength * .20f)
+                {
+                    bodySprite.color = Color.white;
+                    headSprite.color = Color.white;
+                }
+                else if (flashCounter > flashLength * .15f)
+                {
+                    bodySprite.color = Color.green;
+                    headSprite.color = Color.green;
+                }
+                else if (flashCounter > flashLength * .10f)
+                {
+                    bodySprite.color = Color.white;
+                    headSprite.color = Color.white;
+                }
+                else if (flashCounter > flashLength * .05f)
+                {
+                    bodySprite.color = Color.green;
+                    headSprite.color = Color.green;
+                }
+                else
+                {
+                    bodySprite.color = Color.white;
+                    headSprite.color = Color.white;
+                    flashActive = false;
+                }
+                flashCounter -= Time.deltaTime;
+            }
+        }
     }
 
     void ChangeCharacterFunction()
@@ -57,7 +179,9 @@ public class EnermyChangeController : MonoBehaviour
             characterCollection[characterIndex_2].gameObject.SetActive(true);
 
             changeCharacterActive = false;
-		}
+            flashActive = true;
+            flashCounter = flashLength;
+        }
 
-	}
+    }
 }
