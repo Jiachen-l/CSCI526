3014e275c637cf82470fe2210ef950bb22ccd7f2
tonyshaw-usc <70358265+tonyshaw-usc@users.noreply.github.com>
2021-05-02 15:11:45 +0800

    instruction
    the instruction level

---
+++ Assets/c#/InstructionCoin.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InstructionCoin : MonoBehaviour
+{
+    public int coinValue = 1;
+    public GameObject coinDialog;
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            coinDialog.SetActive(false);
+            coinDialog.SetActive(true);
+            ScoreManager.instance.ChangeScore(coinValue);
+            ApplicationData.coinsGetThisLevel++;
+            ApplicationData.coinsLeftThisLevel++;
+            Destroy(this.gameObject);
+
+        }
+    }
+}
---
+++ Assets/c#/InstructionEnemy.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InstructionEnemy : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public GameObject eneDialog;
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            eneDialog.SetActive(true);
+        }
+    }
+}
