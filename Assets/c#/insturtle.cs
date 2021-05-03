39bffc239926847c7e5051cc080f33d58c15349a
tonyshaw-usc <70358265+tonyshaw-usc@users.noreply.github.com>
2021-05-03 23:24:36 +0800

    turtle放到instruction里了
    还是不太知道怎么在turtle上加对话框。。。

---
+++ Assets/c#/insturtle.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class insturtle : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+
+    public GameObject eneDialog;
+    public int damage = 1;
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            eneDialog.SetActive(false);
+            eneDialog.SetActive(true);
+
+        }
+    }
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
