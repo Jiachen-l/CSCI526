63e7ef80fc11004be7f28b9f9476ce4205437566
tonyshaw-usc <70358265+tonyshaw-usc@users.noreply.github.com>
2021-05-04 00:23:17 +0800

    略作调整

--- Assets/c#/LoseCondition.cs
+++ Assets/c#/LoseCondition.cs
@@ -5,6 +5,18 @@ using UnityEngine;
 public class LoseCondition : MonoBehaviour
 {
     public int damage = 1;
+    public GameObject eneDialog;
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            if (eneDialog != null) {
+                eneDialog.SetActive(false);
+                eneDialog.SetActive(true);
+            }
+
+        }
+    }
     // Start is called before the first frame update
     void Start()
     {
